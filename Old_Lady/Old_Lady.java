9fa1e2d79025dd63a84a6141082b306552e38792
unknown <atdp4-ae@105-6.eecs.berkeley.edu>
2015-06-22 11:48:52 -0700

    Pushing file from day 1

---
+++ Old_Lady/Old_Lady.java
@@ -0,0 +1,43 @@
+package Old_Lady;
+
+public class Old_Lady {
+/*author=Rahul Swaminathan
+ * 6/22/15
+ * first day old lady lab
+ */
+	public static void main(String[] args) {
+		System.out.println("There was an old lady who swallowed a fly.");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she’ll die.");
+		System.out.println("There was an old lady who swallowed a spider,");
+		System.out.println("That wriggled and iggled and jiggled inside her.");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
+		System.out.println("There was an old lady who swallowed a bird,");
+		System.out.println("How absurd to swallow a bird.");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
+		System.out.println("There was an old lady who swallowed a cat,");
+		System.out.println("Imagine that to swallow a cat.");
+		System.out.println("She swallowed the cat to catch the bird,");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
+		System.out.println("There was an old lady who swallowed a dog,");
+		System.out.println("What a hog to swallow a dog.");
+		System.out.println("She swallowed the dog to catch the cat,");
+		System.out.println("She swallowed the cat to catch the bird,");
+		System.out.println("She swallowed the bird to catch the spider,");
+		System.out.println("She swallowed the spider to catch the fly,");
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.");
+		System.out.println("There was an old lady who swallowed a horse,");
+		System.out.println("She died of course.");
+		
+	}
+
+}
---
+++ hello_world/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package hello_world;
+
+/**
+ * @author Rahul
+ *6/22/2015. First day of class
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// print hello world
+		
+		System.out.println ("Hello Rahul. Nice to meet you. You are awesome.");
+		System.out.println ();
+		System.out.println ("Bye");
+
+	}
+
+}
